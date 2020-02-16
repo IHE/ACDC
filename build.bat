1b3f2496a79a1af56f9c8cb22e41057147128366
kboone <kboone@ainq.com>
2020-02-15 23:55:11 -0500

    Adding QuestionnaireResponse Example and further cleanup.

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 call sushi .
 xcopy build\input\resources\* input\resources /Y
 xcopy build\input\pagecontent\* input\pagecontent /Y
+xcopy build\input\resource-docs\* input\resource-docs /Y
 copy build\input\ImplementationGuide-*.json input
