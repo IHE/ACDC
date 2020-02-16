ad4c26158a42bc504606ec79e049cbe5af1cb273
kboone <kboone@ainq.com>
2020-02-15 21:31:26 -0500

    Added PHQ-2 Example Questionnaire

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call sushi .
-xcopy build\input\resources\* input\resources 
-xcopy build\input\pagecontent\* input\pagecontent 
+xcopy build\input\resources\* input\resources /Y
+xcopy build\input\pagecontent\* input\pagecontent /Y
 copy build\input\ImplementationGuide-*.json input
