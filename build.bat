e7a164992af69a767830e048e81fd9e187b4826a
kboone <kboone@ainq.com>
2020-02-16 00:26:48 -0500

    Fix for duplicate generation.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 call sushi .
-xcopy build\input\resources\* input\resources /Y
-xcopy build\input\pagecontent\* input\pagecontent /Y
-xcopy build\input\resource-docs\* input\resource-docs /Y
-copy build\input\ImplementationGuide-*.json input
+REM xcopy build\input\resources\* input\resources /Y
+REM xcopy build\input\pagecontent\* input\pagecontent /Y
+REM xcopy build\input\resource-docs\* input\resource-docs /Y
+REM copy build\input\ImplementationGuide-*.json input
